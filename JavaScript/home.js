9c6059f212fb473f281742a40c25707802ff01fe
MillerDix <zlw2579@gmail.com>
2017-04-13 01:02:05 +0800

    windows environment set

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -15,6 +15,29 @@
         let el_greeting = document.getElementById("greeting");
         let clockString = "";
         let greetingString = "";
+        function real_time() {
+            let date = new Date();
+            // clock补0
+            let hours = date.getHours().toString().length < 2 ? "0" + date.getHours().toString() : date.getHours().toString();
+            let minutes = date.getMinutes().toString().length < 2 ? "0" + date.getMinutes().toString() : date.getMinutes().toString();
+
+            if (hours+":"+minutes !== clockString) {
+                el_clock.innerHTML = hours + ":" + minutes;
+                clockString = hours+":"+minutes;
+            }else{
+                return;
+            }
+
+            // greetings
+            if (date.getHours() >= 0 && date.getHours() <= 11) {
+                greetingString = "morning";
+            }else if (date.getHours() >= 12 && date.getHours() <= 18) {
+                greetingString = "afternoon";
+            }else{
+                greetingString = "evening";
+            }
+            el_greeting.innerHTML = "Good " + greetingString + "," + " MillerD.";
+        }
         // 时间,问候语计算
         real_time();
         setInterval(function(){
